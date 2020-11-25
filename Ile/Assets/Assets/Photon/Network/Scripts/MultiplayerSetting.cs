e401456288925fac1a4ba8f2a27c81cf492ae3c3
Diane <diane.bellarbi-salah@epita.fr>
2020-11-26 00:03:51 +0100

    Ajout de c#
    commit

---
+++ Ile/Assets/Assets/Photon/Network/Scripts/MultiplayerSetting.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MultiplayerSetting : MonoBehaviour
+{
+    public static MultiplayerSetting multiplayerSetting;
+
+    public bool delayStart;
+    public int maxPlayers;
+
+    public int menuScene;
+    public int multiplayerScene;
+
+    private void Awake()
+    {
+        if (multiplayerSetting==null)
+        {
+            multiplayerSetting = this;
+        }
+        else
+        {
+            if (multiplayerSetting != this)
+            {
+                Destroy(gameObject);
+            }
+        }
+        DontDestroyOnLoad(gameObject);
+    }
+}
